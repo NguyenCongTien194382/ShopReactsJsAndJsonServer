4e77ffe8018744a1cdd5f2d4febe5bb20e68fe6b
Nguyen Quoc An <an567008@gmail.com>
2021-10-29 11:28:29 +0700

    add_pay

--- src/component/Home/Cart.js
+++ src/component/Home/Cart.js
@@ -1,38 +1,105 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useRef } from "react";
 import { ProductContext } from "../../Context/ProductContext";
 import { CLEAN_CART } from "../../const";
 import CartItem from "./CartItem";
 import { Link } from "react-router-dom";
+import { API_URL } from "../../const";
+import { v4 as uuidv4 } from "uuid";
 
 function Cart() {
   const {
     ProductState: { cart },
-    dispatch,
+    // dispatch,
   } = useContext(ProductContext);
 
   const payCart = () => {
-    // setShowModal(true);
+    setShowModal(true);
     // console.log(JSON.stringify(cart));
-    dispatch({ type: CLEAN_CART, payload: [] });
+    // dispatch({ type: CLEAN_CART, payload: [] });
   };
 
   const [showModal, setShowModal] = useState(false);
+
+  const random = uuidv4();
+
+  const totalPrice = (array) => {
+    return array.reduce((total, item) => {
+      return (total += Number(item.price * item.quanlity));
+    }, 0);
+  };
+
+  const tong_don_hang = totalPrice(cart);
 
   const [data, setData] = useState({
     email: "",
     name: "",
     address: "",
     phone: "",
+    ma_don_hang: random,
+    price: tong_don_hang,
+    status: "0",
   });
 
   const handleOnChangeModal = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
   };
 
+  const luu_chi_tiet_don_hang = () => {
+    const detailsCart = cart.map((item) => {
+      return item;
+    });
+
+    const data_fecth_api = {
+      ma_don_hang: data.ma_don_hang,
+      detailsCart: detailsCart,
+    };
+
+    const option = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(data_fecth_api),
+    };
+
+    fetch(`${API_URL}/detailsCart`, option)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  const luu_don_hang = () => {
+    const option = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(data),
+    };
+
+    fetch(`${API_URL}/historyBuyProducts`, option)
+      .then((res) => res.json())
+      .then((data) => {
+        if (typeof data === "object") {
+          luu_chi_tiet_don_hang();
+        }
+      });
+  };
+
   const handleOnSubmit = (e) => {
     e.preventDefault();
 
-    console.log(data);
+    luu_don_hang();
+
+    setData({
+      ...data,
+      email: "",
+      name: "",
+      address: "",
+      phone: "",
+      ma_don_hang: random,
+    });
   };
 
   return (
@@ -50,12 +117,7 @@ function Cart() {
         )}
 
         <div className="pay">
-          <h1>
-            Tổng cộng: $
-            {cart.reduce((total, item) => {
-              return (total += Number(item.price * item.quanlity));
-            }, 0)}
-          </h1>
+          <h1>Tổng cộng: $ {tong_don_hang}</h1>
 
           <div className="pay-to-win" onClick={payCart}>
             Thanh toán
@@ -78,6 +140,7 @@ function Cart() {
               name="email"
               required
               onChange={handleOnChangeModal}
+              value={data.email}
             />
 
             <label htmlFor="psw">
@@ -88,6 +151,7 @@ function Cart() {
               name="name"
               required
               onChange={handleOnChangeModal}
+              value={data.name}
             />
 
             <label htmlFor="psw">
@@ -98,6 +162,7 @@ function Cart() {
               name="address"
               required
               onChange={handleOnChangeModal}
+              value={data.address}
             />
 
             <label htmlFor="psw">
@@ -108,9 +173,15 @@ function Cart() {
               name="phone"
               required
               onChange={handleOnChangeModal}
+              value={data.phone}
             />
 
-            <button type="submit" onClick={() => setShowModal(false)}>
+            <button
+              type="submit"
+              onClick={() => {
+                setShowModal(false);
+              }}
+            >
               Xác nhận đơn hàng
             </button>
           </div>
